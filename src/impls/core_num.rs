3bb1f1788dd4f3c2c85bbefcfc35daf03c38d834
Kevin Reid <kpreid@switchb.org>
2022-02-15 21:19:20 -0800

    impl Exhaust for FpCategory

--- src/impls/core_num.rs
+++ src/impls/core_num.rs
@@ -1,3 +1,16 @@
-use crate::patterns::impl_newtype_generic;
+use core::num;
 
-impl_newtype_generic!(T: [], core::num::Wrapping<T>, core::num::Wrapping);
+use crate::patterns::{impl_newtype_generic, impl_via_array};
+
+impl_via_array!(
+    num::FpCategory,
+    [
+        Self::Nan,
+        Self::Infinite,
+        Self::Zero,
+        Self::Subnormal,
+        Self::Normal,
+    ]
+);
+
+impl_newtype_generic!(T: [], num::Wrapping<T>, num::Wrapping);
