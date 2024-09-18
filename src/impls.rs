19c019746d2c82404ca7e96e6b0d853248335676
Kevin Reid <kpreid@switchb.org>
2024-09-17 19:40:05 -0700

    Move non-implementation docs from `impls` to the trait.
    A future version may eliminate the `impls` module, and this way, the
    list of non-implementations is next to the list of implementations.

--- src/impls.rs
+++ src/impls.rs
@@ -4,38 +4,6 @@
 //! [`Iterator`]. These need to be public, but should mostly be considered an implementation
 //! detail and not need to be used explicitly.
 //!
-//! The following primitive or standard library types do not implement [`Exhaust`] for
-//! particular reasons:
-//!
-//! * References, because there's nowhere to stash the referent.
-//!   (This could be changed for small finite types, like `&bool`, but those are the same
-//!   sort of types which are unlikely to be used by reference.)
-//! * Pointers, for the same reason as references (and we could generate invalid pointers,
-//!   but that would be almost certainly pointless).
-//! * [`u64`], [`i64`], and [`f64`], because they are too large to feasibly exhaust.
-//! * Types which do not implement [`Clone`]:
-//!
-//!   * [`core::cell::UnsafeCell`]
-//!   * [`std::sync::Mutex`] and `RwLock`
-//!   * [`core::sync::atomic::Atomic*`](core::sync::atomic)
-//!
-//!   A future version of the library might relax the [`Clone`] bound,
-//!   but this is currently impossible.
-//! * Containers that permit duplicate items, and can therefore be unboundedly large:
-//!   * [`alloc::vec::Vec`]
-//!   * [`alloc::collections::VecDeque`]
-//!   * [`alloc::collections::LinkedList`]
-//!   * [`alloc::collections::BinaryHeap`]
-//!
-//! * [`core::mem::ManuallyDrop`], because it would be a memory leak.
-//! * [`core::mem::MaybeUninit`], because it is not useful to obtain a `MaybeUninit<T>`
-//!   value without knowing whether it is initialized, and if they are to be all
-//!   initialized, then `T::exhaust()` is just as good.
-//! * [`core::ops::Range*`](core::ops), because it is ambiguous whether inverted (start > end)
-//!   ranges should be generated.
-//! * [`std::io::ErrorKind`] and other explicitly non-exhaustive types.
-//! * [`std::io::Stdout`] and other types whose sole use is in performing IO.
-//!
 //! [`Exhaust`]: crate::Exhaust
 
 // Impls organized by the corresponding standard library module.
