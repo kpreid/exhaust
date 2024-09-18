f9e737032d1adcc7bf39f8bd1d09724b46aec60b
Kevin Reid <kpreid@switchb.org>
2024-09-17 18:20:57 -0700

    Implement `Exhaust` for non-clonable `std::io` and `std::sync` types.

--- src/impls/core_cell.rs
+++ src/impls/core_cell.rs
@@ -2,4 +2,4 @@ use crate::patterns::impl_newtype_generic;
 
 impl_newtype_generic!(T: [Copy], core::cell::Cell<T>, core::cell::Cell::new);
 impl_newtype_generic!(T: [], core::cell::RefCell<T>, core::cell::RefCell::new);
-// Cannot impl for UnsafeCell because it is not Clone.
+impl_newtype_generic!(T: [], core::cell::UnsafeCell<T>, core::cell::UnsafeCell::new);
