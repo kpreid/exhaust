01865050ef239d1defb34dd3891f426adee77e04
Kevin Reid <kpreid@switchb.org>
2024-09-23 08:02:23 -0700

    Remove no-longer-needed `Copy` bound on `impl Exhaust for Cell`.

--- src/impls/core_cell.rs
+++ src/impls/core_cell.rs
@@ -1,5 +1,5 @@
 use crate::patterns::impl_newtype_generic;
 
-impl_newtype_generic!(T: [Copy], core::cell::Cell<T>, core::cell::Cell::new);
+impl_newtype_generic!(T: [], core::cell::Cell<T>, core::cell::Cell::new);
 impl_newtype_generic!(T: [], core::cell::RefCell<T>, core::cell::RefCell::new);
 impl_newtype_generic!(T: [], core::cell::UnsafeCell<T>, core::cell::UnsafeCell::new);
