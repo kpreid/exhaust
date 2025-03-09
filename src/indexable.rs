95e18f63f141462d909a63dce81216b465b65434
Kevin Reid <kpreid@switchb.org>
2025-03-08 18:19:06 -0800

    DRAFT: Add `Indexable` trait. Needs proving in usage.

---
+++ src/indexable.rs
@@ -0,0 +1,19 @@
+use crate::Exhaust;
+
+/// Types whose values can be mapped to integer indices.
+pub trait Indexable: Exhaust<Iter: ExactSizeIterator> {
+    /// Number of distinct values of this type.
+    /// Equivalent to `Self::exhaust().len()`, but is a constant.
+    const VALUE_COUNT: usize;
+
+    /// Returns the position within `Self::exhaust()` that `value` may be found.
+    ///
+    /// Equivalent to `Self::exhaust().position(value).unwrap()`, but more efficient;
+    /// note that a correct implementation cannot panic.
+    fn to_index(value: &Self) -> usize;
+
+    /// Equivalent to `Self::exhaust().nth(index).unwrap()`.
+    ///
+    /// Panics if `index >= Self::VALUE_COUNT`.
+    fn from_index(index: usize) -> Self;
+}
