d07ed2d11fbd45f3a4228dae47c30026410a2918
Kevin Reid <kpreid@switchb.org>
2025-05-01 11:26:45 -0700

    `impl Indexable for Infallible`

--- src/impls/core_convert.rs
+++ src/impls/core_convert.rs
@@ -1,6 +1,6 @@
 use core::iter;
 
-use crate::Exhaust;
+use crate::{Exhaust, Indexable};
 
 impl Exhaust for core::convert::Infallible {
     type Iter = iter::Empty<core::convert::Infallible>;
@@ -8,4 +8,19 @@ impl Exhaust for core::convert::Infallible {
         iter::empty()
     }
     crate::patterns::factory_is_self!();
+}
+
+impl Indexable for core::convert::Infallible {
+    const VALUE_COUNT: usize = 0;
+
+    #[mutants::skip]
+    fn to_index(value: &Self) -> usize {
+        match *value {}
+    }
+
+    #[track_caller]
+    #[mutants::skip]
+    fn from_index(_: usize) -> Self {
+        panic!("core::convert::Infallible has no valid indices")
+    }
 }
