d24624dda8146be48f59d6b63f3214da7cd19c85
Kevin Reid <kpreid@switchb.org>
2025-05-01 12:24:37 -0700

    `impl Indexable for Cell<T>`

--- src/impls/core_cell.rs
+++ src/impls/core_cell.rs
@@ -1,11 +1,23 @@
 use core::cell;
 
 use crate::patterns::{delegate_factory_and_iter, impl_newtype_generic};
-use crate::Exhaust;
+use crate::{Exhaust, Indexable};
 
 impl_newtype_generic!(T: [], cell::Cell<T>, cell::Cell::new);
 impl_newtype_generic!(T: [], cell::RefCell<T>, cell::RefCell::new);
 impl_newtype_generic!(T: [], cell::UnsafeCell<T>, cell::UnsafeCell::new);
+
+impl<T: Copy + Indexable> Indexable for cell::Cell<T> {
+    const VALUE_COUNT: usize = T::VALUE_COUNT;
+
+    fn to_index(value: &Self) -> usize {
+        T::to_index(&value.get())
+    }
+
+    fn from_index(index: usize) -> Self {
+        cell::Cell::new(T::from_index(index))
+    }
+}
 
 impl<T: Exhaust> Exhaust for cell::OnceCell<T> {
     delegate_factory_and_iter!(Option<T>);
